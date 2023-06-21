fc398a76f776cb45fccf3bb93a3eaa26055230a5
IskandarGIR <bcrhf2003bcrhf@mail.ru>
2023-06-21 05:12:56 +0300

    add permissions

--- books/store/tests/test_api.py
+++ books/store/tests/test_api.py
@@ -1,5 +1,9 @@
+import json
+
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
+# from rest_framework.authtoken.admin import User
 from rest_framework.test import APITestCase
 
 from store.models import Book
@@ -8,8 +12,10 @@ from store.serializers import BooksSerializer
 
 class BooksApiTestCase(APITestCase):
     def setUp(self):
+        self.user = User.objects.create(username='test_username')
         self.book_1 = Book.objects.create(name='Test book 1', price='25.00',
-                                          author_name='Author 1')
+                                          author_name='Author 1',
+                                          owner=self.user)
         self.book_2 = Book.objects.create(name='Test book 2', price='55.00',
                                           author_name='Author 2')
         self.book_3 = Book.objects.create(name='Test book Author 1', price='55.00',
@@ -29,10 +35,26 @@ class BooksApiTestCase(APITestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
-
     def test_get_search(self):
         url = reverse('book-list')
         response = self.client.get(url, data={'search': 'Author 1'})
         serializer_data = BooksSerializer([self.book_1, self.book_3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
+
+    def test_create(self):
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-list')
+        data = {
+            'name': 'Programming in Python 3',
+            'price': 150,
+            'author_name': 'Mark'
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.post(url, data=json_data,
+                                    content_type='application/json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(4, Book.objects.all().count())
+        self.assertEqual(self.user, Book.objects.last().owner)
+
--- books/store/views.py
+++ books/store/views.py
@@ -2,9 +2,10 @@ import django_filters
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from store.models import Book
+from store.permissions import IsOwnerOrStaffOrReadOnly
 from store.serializers import BooksSerializer
 
 
@@ -13,9 +14,13 @@ class BookViewSet(ModelViewSet):
     serializer_class = BooksSerializer
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['price']
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwnerOrStaffOrReadOnly]
     search_fields = ['name', 'author_name']
     ordering_fields = ['price', 'author_name']
+
+    def perform_create(self, serializer):
+        serializer.validated_data['owner'] = self.request.user
+        serializer.save()
 
 
 def auth(request):
